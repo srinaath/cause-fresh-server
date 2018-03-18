0fc728986a7ed915f16822d5b528f19064e30448
Srinaath Ravichandran <sravichandran@deloitte.com>
2018-03-18 03:25:32 -0600

    More updates to public
    Signed-off-by: Srinaath Ravichandran <sravichandran@deloitte.com>

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ const serverUtils = registerServerHandlers(app);
 serverUtils.setEnvVariables(process.env.NODE_ENV);
 
 serverUtils.initExpressUtilities();
+app.use('/', express.static('./public'));
 
-app.use(express.static('public'));
 
 app.listen(app.get('port'), () => {
   console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
